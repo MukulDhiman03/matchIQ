dc1b0729febd72bdbe5e878018d0fc7a7f754a40
MukulDhiman03 <dhimanmukul307@gmail.com>
2025-07-20 17:42:45 +0530

    Dynamic Routes

--- src/app.js
+++ src/app.js
@@ -1,28 +1,13 @@
 const express = require("express");
 const app = express();
 
-// this will match all the HTTP method API calls to /test
-app.use("/test", (req, res) => {
-  res.send("Hello from server!");
-});
-
-app.use("/user", (req, res) => {
-  res.send("HAHAHAHAH");
-});
-
-// this will only handle the GET call to /user
-app.get("/user", (req, res) => {
+app.get("/user/:userId", (req, res) => {
+  console.log(req.params);
   res.send({ firstName: "Mukul", lastName: "Dhiman" });
 });
-
-app.post("/user", (req, res) => {
-  console.log("Saved data to database");
-  res.send("Data saved to database");
-});
-
-app.delete("/user", (req, res) => {
-  console.log("Deleted data to database");
-  res.send("Data Deleted");
+app.get("/user", (req, res) => {
+  console.log(req.query);
+  res.send({ firstName: "Mukul", lastName: "Dhiman" });
 });
 
 app.listen(3000, () => {
