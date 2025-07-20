3c4dade56c7cf266ecda9e06746c1772712d036d
MukulDhiman03 <dhimanmukul307@gmail.com>
2025-07-20 17:31:50 +0530

    Use Get Post Delete

--- src/app.js
+++ src/app.js
@@ -1,14 +1,28 @@
 const express = require("express");
 const app = express();
 
-app.use("/", (req, res) => {
-  res.send("Hello from home page!");
-});
+// this will match all the HTTP method API calls to /test
 app.use("/test", (req, res) => {
   res.send("Hello from server!");
 });
-app.use("/hello", (req, res) => {
-  res.send("Hello from hello!");
+
+app.use("/user", (req, res) => {
+  res.send("HAHAHAHAH");
+});
+
+// this will only handle the GET call to /user
+app.get("/user", (req, res) => {
+  res.send({ firstName: "Mukul", lastName: "Dhiman" });
+});
+
+app.post("/user", (req, res) => {
+  console.log("Saved data to database");
+  res.send("Data saved to database");
+});
+
+app.delete("/user", (req, res) => {
+  console.log("Deleted data to database");
+  res.send("Data Deleted");
 });
 
 app.listen(3000, () => {
