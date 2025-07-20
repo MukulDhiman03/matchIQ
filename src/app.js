5123922b3611d56161dcc7b827b7d6761410aa82
MukulDhiman03 <dhimanmukul307@gmail.com>
2025-07-20 22:02:06 +0530

    Created Schema and model

--- src/app.js
+++ src/app.js
@@ -1,7 +1,24 @@
 const express = require("express");
 const connectDB = require("./config/database");
-
 const app = express();
+const User = require("./models/user");
+
+app.post("/signup", async (req, res) => {
+  // instance of user model
+  const user = new User({
+    firstName: "Virat",
+    lastName: "Kohli",
+    emailId: "Viratkohli@gmail.com",
+    password: "password@1234",
+  });
+
+  try {
+    await user.save();
+    res.send("Added to database");
+  } catch (err) {
+    res.status(400).send("Error saving the user:" + err.message);
+  }
+});
 
 connectDB()
   .then(() => {
