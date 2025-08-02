105a10469035b723ae4a5bb423dcd535385e308b
MukulDhiman03 <dhimanmukul307@gmail.com>
2025-08-02 22:01:48 +0530

    Add: Accept request api

--- src/routes/requests.js
+++ src/routes/requests.js
@@ -13,8 +13,8 @@ requestRouter.post(
       const fromUserId = req.user._id;
       const toUserId = req.params.toUserId;
       const status = req.params.status;
-      console.log(fromUserId);
-      console.log(toUserId);
+      // console.log(fromUserId);
+      // console.log(toUserId);
 
       if (fromUserId.equals(toUserId)) {
         return res.status(400).json({
@@ -72,6 +72,46 @@ requestRouter.post(
               status +
               " " +
               ifToUserExists.firstName,
+        data,
+      });
+    } catch (err) {
+      res.status(400).send(err.message);
+    }
+  }
+);
+requestRouter.post(
+  "/request/review/:status/:requestId",
+  userAuth,
+  async (req, res) => {
+    try {
+      const { status, requestId } = req.params;
+      const loggedInUser = req.user;
+
+      const allowedStatus = ["accepted", "rejected"];
+      if (!allowedStatus.includes(status)) {
+        return res.status(400).json({
+          message: "Invalid status",
+        });
+      }
+
+      const connectionRequest = await connectionRequestModel.findOne({
+        _id: requestId,
+        toUserId: loggedInUser._id,
+        status: "interested",
+      });
+
+      if (!connectionRequest) {
+        return res.status(404).json({
+          message: "Connection request not found.",
+        });
+      }
+
+      connectionRequest.status = status;
+
+      const data = await connectionRequest.save();
+
+      res.json({
+        message: "Request accepted",
         data,
       });
     } catch (err) {
