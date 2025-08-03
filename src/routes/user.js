e9089a997a72b6f28d3a5fc8ded5eda8518aa794
MukulDhiman03 <dhimanmukul307@gmail.com>
2025-08-03 12:33:07 +0530

    Add: feed api

--- src/routes/user.js
+++ src/routes/user.js
@@ -3,6 +3,16 @@ const express = require("express");
 const userRouter = express.Router();
 const { userAuth } = require("../middlewares/auth");
 const ConnectionRequest = require("../models/connectionRequest");
+const User = require("../models/user");
+const USER_SAFE_DATA = [
+  "firstName",
+  "lastName",
+  "photoUrl",
+  "about",
+  "skills",
+  "age",
+  "gener",
+];
 
 // get all the pending connection request for the loggedIn user
 userRouter.get("/user/requests/received", userAuth, async (req, res) => {
@@ -39,24 +49,8 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
         },
       ],
     })
-      .populate("fromUserId", [
-        "firstName",
-        "lastName",
-        "photoUrl",
-        "about",
-        "skills",
-        "age",
-        "gener",
-      ])
-      .populate("toUserId", [
-        "firstName",
-        "lastName",
-        "photoUrl",
-        "about",
-        "skills",
-        "age",
-        "gener",
-      ]);
+      .populate("fromUserId", USER_SAFE_DATA)
+      .populate("toUserId", USER_SAFE_DATA);
 
     const data = connectionRequest.map((row) => {
       if (row.fromUserId._id.toString() === loggedInUser._id.toString()) {
@@ -73,4 +67,34 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
   }
 });
 
+userRouter.get("/feed", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+
+    // find all connection request(sent + received)
+    const connectionRequest = await ConnectionRequest.find({
+      $or: [{ fromUserId: loggedInUser._id }, { toUserId: loggedInUser._id }],
+    }).select("fromUserId toUserId");
+    // .populate("fromUserId", "firstName")
+    // .populate("toUserId", "firstName");
+
+    const hideUsersFromFeed = new Set();
+    connectionRequest.forEach((req) => {
+      hideUsersFromFeed.add(req.fromUserId.toString());
+      hideUsersFromFeed.add(req.toUserId.toString());
+    });
+
+    // console.log(hideUsersFromFeed);
+    const users = await User.find({
+      $and: [
+        { _id: { $nin: Array.from(hideUsersFromFeed) } },
+        { _id: { $ne: loggedInUser._id } },
+      ],
+    }).select(USER_SAFE_DATA);
+
+    res.send({ data: users });
+  } catch (err) {
+    res.status(400).send("Error: " + err.message);
+  }
+});
 module.exports = userRouter;
